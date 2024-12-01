f8ad0481744339c419a4ace723c8eaf77b6f658d
Ayaka <KiritaniAyaka@users.noreply.github.com>
2024-12-01 13:59:14 +0800

    improve readability

--- day1/part1.ts
+++ day1/part1.ts
@@ -1,7 +1,7 @@
 import { solve } from "@kiritaniayaka/aoc-toolkit";
 
 solve((input) => {
-  const arr = input.split("\n").reduce(
+  const [left, right] = input.split("\n").reduce(
     (acc, line) => {
       const r = /(\d+)\s+(\d+)/g.exec(line)!;
       acc[0].push(+r[1]);
@@ -10,11 +10,11 @@ solve((input) => {
     },
     [[], []] as [number[], number[]]
   );
-  arr[0].sort((a, b) => b - a);
-  arr[1].sort((a, b) => b - a);
+  left.sort((a, b) => b - a);
+  right.sort((a, b) => b - a);
   let sum = 0;
-  while (arr[0].length) {
-    sum += Math.abs(arr[0].pop()! - arr[1].pop()!);
+  while (left.length) {
+    sum += Math.abs(left.pop()! - right.pop()!);
   }
   return sum;
 });
--- day1/part2.ts
+++ day1/part2.ts
@@ -1,7 +1,7 @@
 import { solve } from "@kiritaniayaka/aoc-toolkit";
 
 solve((input) => {
-  const arr = input.split("\n").reduce(
+  const [left, right] = input.split("\n").reduce(
     (acc, line) => {
       const r = /(\d+)\s+(\d+)/g.exec(line)!;
       acc[0].push(+r[1]);
@@ -10,9 +10,9 @@ solve((input) => {
     },
     [[], []] as [number[], number[]]
   );
-  const m = arr[1].reduce(
+  const m = right.reduce(
     (acc, n) => acc.set(n, (acc.get(n) || 0) + 1),
     new Map<number, number>()
   );
-  return arr[0].reduce((acc, cur) => m.get(cur) ?? 0 * cur + acc, 0);
+  return left.reduce((acc, cur) => m.get(cur) ?? 0 * cur + acc, 0);
 });
